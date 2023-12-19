dab5c3a199268bc3378024a83069ac012847cdc3
Laura Schmidt <lauramarie.sdt@gmail.com>
2023-12-19 10:06:52 +0100

    first commit

---
+++ src/contexts/DataContext/index.js
@@ -0,0 +1,63 @@
+import PropTypes from "prop-types";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+const DataContext = createContext({});
+
+export const api = {
+  loadData: async () => {
+    const json = await fetch("/events.json");
+    return json.json();
+  },
+  loadDataLast: async () => {
+    const json2 = await fetch("/events.json");
+    const json3 = await json2.json();
+    const json4 = json3.events.reduce((a, b) => (new Date(a.date) > new Date(b.date) ? a : b));
+    return json4;
+  },
+};
+
+export const DataProvider = ({ children }) => {
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  const [last, setLast] = useState(null);
+  const getData = useCallback(async () => {
+    try {
+      setData(await api.loadData());
+      setLast(await api.loadDataLast());
+
+    } catch (err) {
+      setError(err);
+    }
+  }, []);
+  useEffect(() => {
+    if (data) return;
+    getData();
+  });
+
+  return (
+    <DataContext.Provider
+      // eslint-disable-next-line react/jsx-no-constructed-context-values
+      value={{
+        data,
+        error,
+        last,
+      }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
+};
+
+DataProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export const useData = () => useContext(DataContext);
+
+export default DataContext;
